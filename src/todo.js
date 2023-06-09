0ed9c77833fe5da0b1abc40b2e7151623695ce84
cc0803 <simongeldbach@gmail.com>
2023-06-09 14:55:26 +0200

    Delete old javascript files and make new ones, add createTODO to TODO

---
+++ src/todo.js
@@ -0,0 +1,7 @@
+const createTODO = (title, description, dueDate, priority, category) => {
+    this.title = title,
+    this.description = description,
+    this.dueDate = dueDate,
+    this.priority = priority,
+    this.category = category
+}
--- src/todoObject.js
+++
@@ -1,5 +0,0 @@
-const Todo = (title, description, dueDate, Category, priority) => {
-    return {title, description, dueDate, Category, priority};
-}
-
-export default Todo;
