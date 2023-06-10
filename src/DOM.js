f4e48268efabaf26f9ba9b8ba171460c887c627e
cc0803 <simongeldbach@gmail.com>
2023-06-10 08:45:44 +0200

    Add logic to displayTodos, removeTodos and change Heading

--- src/DOM.js
+++ src/DOM.js
@@ -1,16 +1,29 @@
-function createTodoHTML(title, dueDate, priority) {
+const DOM = () => {
+
+}
+
+
+/* Function for creating the html 
+for every single todo task*/ 
+function createTodoHTML(todo) {
     let container = document.createElement("p");
     container.classList.add("todo");
 
     container.innerHTML = `
-                    <input type="checkbox" name="${title}" id="check">
-                    <span class="task">${title}</span>
+                    <input type="checkbox" name="${todo.title}" id="check">
+                    <span class="task">${todo.title}</span>
                     <span class="spacer"></span>
-                    <span class="priority ${priority}">${priority}</span>
-                    <span class="due">${dueDate}</span>
+                    <span class="priority ${todo.priority}">${todo.priority}</span>
+                    <span class="due">${todo.dueDate}</span>
                     `;
     return container;
 }
+
+/* 
+--createCategoryHTML creates the HTML for the single
+Category button
+-- addCategoryHTML appends this html to the category section
+*/
 
 function createCategoryHTML(name) {
     let button = document.createElement("button");
@@ -21,8 +34,26 @@ function createCategoryHTML(name) {
     return button;
 }
 
-export function addCategoryHTML(name) {
+function addCategoryHTML(name) {
     let html = createCategoryHTML(name);
     let section = document.querySelector(".sidebar section:last-of-type");
     section.appendChild(html);
 } 
+
+// Selecting the todo tasks Container
+const todoContainer = document.querySelector(".todo-container")
+
+function removeContent() {
+    todoContainer.innerHTML = "";
+}
+
+function displayTodos(category) {
+    category.array.forEach(element => {
+        todoContainer.appendChild(createTodoHTML(category));
+    })
+}   
+
+function changeTodoContainerHeading(category) {
+    const heading = document.querySelector(".container>h3");
+    heading.textContent = category.name;
+}
